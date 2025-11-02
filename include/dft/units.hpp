83b892b7d37618f87108c0d97d3d9a4dde9f49a7
Theodoros <83337690+TheodorosPanagiotakopoulos@users.noreply.github.com>
2025-11-02 09:52:28 -0500

    Create units.hpp

---
+++ include/dft/units.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <boost/units/systems/si.hpp>
+#include <boost/units/io.hpp>
+
+namespace dft { namespace units {
+
+struct au {
+  static constexpr double hartree_to_ev = 27.211386245988;
+  static constexpr double bohr_to_ang = 0.529177210903;
+};
+
+inline double ang_to_bohr(double ang) { return ang / au::bohr_to_ang; }
+inline double bohr_to_ang(double bohr){ return bohr * au::bohr_to_ang; }
+
+}} // namespace dft::units
