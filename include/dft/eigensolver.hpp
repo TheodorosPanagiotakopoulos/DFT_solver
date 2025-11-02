0d6f3ad13038edff6f3d9a99e9756ca5005c8d16
Theodoros <83337690+TheodorosPanagiotakopoulos@users.noreply.github.com>
2025-11-02 09:54:02 -0500

    Create eigensolver.hpp

---
+++ include/dft/eigensolver.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include <Eigen/Sparse>
+#include <Eigen/Dense>
+#include <stdexcept>
+
+namespace dft {
+
+struct Eigenpairs {
+  Eigen::VectorXd eigenvalues;
+  Eigen::MatrixXd eigenvectors;
+};
+
+inline Eigenpairs solve_lowest_dense(const Eigen::SparseMatrix<double>& H, int k){
+  Eigen::MatrixXd Hd = Eigen::MatrixXd(H);
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXd> es(Hd);
+  if(es.info()!=Eigen::Success) throw std::runtime_error("eigensolve failed");
+  Eigenpairs out;
+  out.eigenvalues = es.eigenvalues().head(k);
+  out.eigenvectors = es.eigenvectors().leftCols(k);
+  return out;
+}
+
+} // namespace dft
