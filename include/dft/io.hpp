db1c028dc90de1b1415d445c7628a28b367bcab0
Theodoros <83337690+TheodorosPanagiotakopoulos@users.noreply.github.com>
2025-11-02 09:54:43 -0500

    Create io.hpp

---
+++ include/dft/io.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <fstream>
+#include <vector>
+#include <iomanip>
+#include <string>
+
+namespace dft { namespace io {
+
+inline void write_scalar(const std::string& file,
+                         const std::vector<double>& data,
+                         size_t nx,size_t ny,size_t nz)
+{
+  std::ofstream ofs(file);
+  ofs << std::setprecision(10);
+  for(size_t i=0;i<data.size();++i) ofs << data[i] << "\n";
+}
+
+}} // namespace dft::io
