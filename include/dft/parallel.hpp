3dc7b2fc58346036b02f65532029793de92ea2fd
Theodoros <83337690+TheodorosPanagiotakopoulos@users.noreply.github.com>
2025-11-02 09:54:24 -0500

    Create parallel.hpp

---
+++ include/dft/parallel.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
+namespace dft { namespace par {
+
+template<typename F>
+void for_index(size_t N, F f){
+  tbb::parallel_for(tbb::blocked_range<size_t>(0,N),
+    [&](const tbb::blocked_range<size_t>& r){
+      for(size_t i=r.begin(); i<r.end(); ++i) f(i);
+    });
+}
+
+}} // namespace dft::par
